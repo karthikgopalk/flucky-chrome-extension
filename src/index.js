bf63f3fd1b87ebcd8e11c2cf8f8151c7210c7c33
karthikgopalk <riokarthy@gmail.com>
2022-06-15 14:49:07 +0530

    pwa 2

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import PWAPrompt from "react-ios-pwa-prompt";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <PWAPrompt copyTitle="Flucky" />
   </React.StrictMode>
 );
 
