dd8888b989dee0c0bebfa12bd7c221d6996f61e0
karthikgopalk <riokarthy@gmail.com>
2022-06-15 13:13:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Vocab } from "./vocabList";
+const logo = require("./flucky-logo.png");
 
 function App() {
+  const randomNum = Math.floor(Math.random() * Math.floor(Vocab.length));
+  const [randomWords, setRandomWords] = useState(0);
+
+  const randomGenerator = () => {
+    return setRandomWords(randomNum);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-logo">
+          <img src={logo} alt="Flucky logo" width="25px" height="25px" />
+          <p className="header-title">Flucky</p>
+        </div>
+        <div className="main">
+          <h1 className="title">{Vocab[randomNum].FIELD1}</h1>
+          <div className="abjective-holder">
+            <p className="abjective">Abjective:</p>
+            <p className="meaning"> {Vocab[randomNum].FIELD2}</p>
+          </div>
+        </div>
+        <button className="button-64" onClick={randomGenerator}>
+          <span class="text">Flucky</span>
+        </button>
+        <div className="footer">
+          <p className="footer-text">
+            Made with <span role="img">❤️</span> by{" "}
+            <a href="https://karthikgopal.netlify.app/">
+              <strong>Karthik Gopal</strong>
+            </a>
+          </p>
+        </div>
       </header>
     </div>
   );
